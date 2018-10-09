85469c39457cc7c017e9884f90d4183ae7d7236c
Alex Raubach <antigrapist@gmail.com>
2018-10-08 21:35:09 -0400

    New Participants should have a swiss_rank, not an overall rank

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -7,7 +7,7 @@ class Tournament < ApplicationRecord
   def create_empty_squads
     return if participant_number.to_i.zero?
     participant_number.to_i.times do |i|
-      Participant.new(overall_rank: i + 1, tournament_id: id).save
+      Participant.new(swiss_rank: i + 1, tournament_id: id).save
     end
   end
 end
