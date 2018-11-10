5d8f702d9f86fc46b99f1e02a088d0168edde3cd
Alex Raubach <antigrapist@gmail.com>
2018-11-09 20:54:56 -0500

    Allow free form addresses

--- db/migrate/20181108030503_create_addresses.rb
+++ db/migrate/20181108030503_create_addresses.rb
@@ -1,13 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.int :league_player_id
+      t.integer :league_player_id
       t.string :first_line
       t.string :second_line
       t.string :city
       t.string :county_province
       t.string :zip_or_postcode
       t.string :country
+      t.boolean :use_other
       t.string :other
 
       t.timestamps
