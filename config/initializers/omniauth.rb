40c13e879d9fd3ed7e7885433ff1086ac6a262cb
Alex Raubach <antigrapist@gmail.com>
2018-11-12 00:53:14 -0500

    Disable sign in with google until multiple identity loggin is solid

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['google_client_id'], ENV['gooogle_client_secret']
+  #provider :google_oauth2, ENV['google_client_id'], ENV['gooogle_client_secret']
+  provider :slack, ENV['slack_client_id'], ENV['slack_client_secret'], scope: 'identity.basic identity.email identity.team identity.avatar', team: 'T2FCSKL3Z'
 end
