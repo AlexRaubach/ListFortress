bbae7d79b18c510e0e5183e0e90252cd07da6f09
Alex Raubach <antigrapist@gmail.com>
2018-11-12 23:18:01 -0500

    Move changes to create users migration to a seperate migration

--- db/migrate/20181004023419_create_users.rb
+++ db/migrate/20181004023419_create_users.rb
@@ -1,14 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :provider
+      t.string :uid
       t.string :email
       t.string :first_name
       t.string :last_name
-      t.boolean :league_eligible
-      t.string :nickname
-      t.string :slack_avatar
-      t.boolean :admin
-
+      t.index :uid
+      
       t.timestamps
     end
   end
