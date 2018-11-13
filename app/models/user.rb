cb5be95ece3f5af656ff202cb728b5bb599a5b0b
Alex Raubach <antigrapist@gmail.com>
2018-11-12 23:29:12 -0500

    Change user model to consistantly use safe access

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ApplicationRecord
   has_many :identities
   has_many :league_participants
 
-
   def self.create_with_omniauth(auth)
     user = User.create(
-      nickname: auth['info']&.name,
-      email: auth['info']&.email,
+      name: auth&.info&.name,
+      email: auth&.info&.email
     )
 
     if auth['provider'] == 'slack'
-      user.slack_avatar = auth['info']&.image
+      user.slack_avatar = auth&.info&.image
       user.league_eligible = true
+      user.slack_id = auth&.info&.user_id
     end
 
     user.save
