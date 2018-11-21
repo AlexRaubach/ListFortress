90349c48b49f856f7cd2ee122eeaebe87947e1a6
Alex Raubach <antigrapist@gmail.com>
2018-11-21 13:38:27 -0500

    Fix Participant counter cache name

--- db/schema.rb
+++ db/schema.rb
@@ -135,7 +135,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_020916) do
     t.boolean "locked"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "participants_cache"
+    t.integer "participants_count", default: 0, null: false
   end
 
   create_table "upgrades", force: :cascade do |t|
