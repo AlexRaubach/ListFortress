d268ad86680984dc06b368f492ae3fb18b7a7a04
Alex Raubach <antigrapist@gmail.com>
2018-12-17 00:11:44 -0500

    Create Divisions

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,2 +1,3 @@
 class Division < ApplicationRecord
+  belongs_to :season
 end
--- db/migrate/20181109003516_create_divisions.rb
+++ db/migrate/20181109003516_create_divisions.rb
@@ -1,6 +1,10 @@
 class CreateDivisions < ActiveRecord::Migration[5.2]
   def change
     create_table :divisions do |t|
+      t.string :name
+      t.integer :season_id
+      t.integer :tier
+      
 
       t.timestamps
     end
