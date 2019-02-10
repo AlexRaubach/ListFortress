b5db4c0e06c4d75efc9278367d970cb9557024a2
Alex Raubach <antigrapist@gmail.com>
2019-02-09 21:24:58 -0500

    Fix match associations and change them to be polymorphic

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,10 +1,10 @@
 class Match < ApplicationRecord
-  belongs_to :round
-  has_one :player1, :class_name => 'Participant', :foreign_key => 'player1_id'
-  has_one :player2, :class_name => 'Participant', :foreign_key => 'player2_id'
-  has_one :winner, :class_name => 'Participant', :foreign_key => 'winner_id'
+  belongs_to :round, optional: true
+  belongs_to :player1, polymorphic: true, optional: true
+  belongs_to :player2, polymorphic: true, optional: true
+  belongs_to :winner, polymorphic: true, optional: true
 
   def serializable_hash(options={})
-    super({:only => [:id,:player1_id,:player1_points,:player2_id,:player2_points,:result, :winner_id]}.merge(options||{}))
+    super({only: [:id, :player1_id, :player1_points, :player2_id, :player2_points, :result, :winner_id]}.merge(options||{}))
   end
 end
