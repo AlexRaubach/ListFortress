74034c8f7db2561094e282a3b9bf8656a1ba2498
antigrapist <antigrapist@gmail.com>
2019-02-18 22:41:32 -0500

    Deletes duplicate routes and fixes linting errors

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,17 +4,6 @@ class MatchesController < ApplicationController
   # GET /matches
   # GET /matches.json
   def index
-    @matches = Match.all
-  end
-
-  # GET /matches/1
-  # GET /matches/1.json
-  def show
-    @match = Match.find_by(params[:id])
-    
-  end
-
-  def index
     @matches = Match.all.paginate(page: params[:page], per_page: 25)
   end
 
@@ -22,9 +11,9 @@ class MatchesController < ApplicationController
   # GET /tournaments/1.json
   def show
     respond_to do |format|
-      #@match = Match.where(id:params[:id])
-      format.html 
-      format.csv { send_data  Tournament.where(id:params[:id]).to_csv, filename: "listfortress-#{@tournament.id}.csv"}
+      # @match = Match.where(id:params[:id])
+      format.html
+      format.csv { send_data  Match.where(id: params[:id]).to_csv, filename: "listfortress-#{@tournament.id}.csv"}
     end
   end
 
@@ -71,33 +60,33 @@ class MatchesController < ApplicationController
   end
 
   private
-    def update_parents(match)
-      round = Round.find_by(id:match.round_id)
-        if round.present?
-          round.touch
-          tourney = Tournament.find_by(id:round.tournament_id)
-          if tourney.present?
-            tourney.touch
-          end
-        end
-    end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_match
-      @match = Match.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
-        render(:file => File.join(Rails.root, 'public/404.html'), :status => 404, :layout => false)
-      # handle not found error
-      rescue ActiveRecord::ActiveRecordError
-        render(:file => File.join(Rails.root, 'public/404.html'), :status => 404, :layout => false)
-      # handle other ActiveRecord errors
-      rescue StandardError
-        render(:file => File.join(Rails.root, 'public/404.html'), :status => 404, :layout => false)
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def match_params
-      params.permit(:id, match:
-      [:player1_id, :player1_points, :player2_id, :player2_points, :result, :round_id, :winner_id]
-    )
+  def update_parents(match)
+    round = Round.find_by(id: match.round_id)
+    if round.present?
+      round.touch
+      tourney = Tournament.find_by(id: round.tournament_id)
+      tourney.touch if tourney.present?
     end
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_match
+    @match = Match.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render(file: File.join(Rails.root, 'public/404.html'), status: 404, layout: false)
+    # handle not found error
+  rescue ActiveRecord::ActiveRecordError
+    render(file: File.join(Rails.root, 'public/404.html'), status: 404, layout: false)
+    # handle other ActiveRecord errors
+  rescue StandardError
+    render(file: File.join(Rails.root, 'public/404.html'), status: 404, layout: false)
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def match_params
+    params.permit(:id, match:
+    [:player1_id, :player1_points, :player2_id, :player2_points, :result, :round_id, :winner_id]
+  )
+  end
 end
