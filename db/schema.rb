5aee5d439c90e0d335d9819b3f7a54ff2bd8b71c
Alex Raubach <Alex@AlexRaubach.com>
2019-02-28 09:20:22 -0500

    Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_190306) do
+ActiveRecord::Schema.define(version: 2019_02_27_125129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_190306) do
     t.integer "tier"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "letter"
   end
 
   create_table "factions", force: :cascade do |t|
@@ -111,6 +112,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_190306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "promotion"
+    t.integer "league_match"
   end
 
   create_table "matches", force: :cascade do |t|
@@ -242,7 +244,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_190306) do
     t.boolean "locked"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "participants_cache"
+    t.integer "participants_count", default: 0, null: false
   end
 
   create_table "upgrades", force: :cascade do |t|
