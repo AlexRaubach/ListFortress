fbab6a1a6dfc0a3488e7b78c7a93b8a17104273a
mwhited <matthew.whited@gmail.com>
2019-03-08 10:16:05 -0800

    Added Player Name to Participants API response @ request of Kerbocat

--- app/serializers/api/v1/participant_serializer.rb
+++ app/serializers/api/v1/participant_serializer.rb
@@ -2,6 +2,7 @@ class Api::V1::ParticipantSerializer < ActiveModel::Serializer
     #attributes(*Participant.attribute_names.map(&:to_sym))
 
     attribute :id
+    attribute :name
     attribute :tournament_id
     attribute :score
     attribute :swiss_rank
