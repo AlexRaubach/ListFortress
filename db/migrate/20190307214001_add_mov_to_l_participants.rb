7edd161130c95d239d0539ec37d1759ab1bb2009
Alex Raubach <Alex@AlexRaubach.com>
2019-03-08 11:34:33 -0500

    Change column name to avoid name collision

--- db/migrate/20190307214001_add_mov_to_l_participants.rb
+++ db/migrate/20190307214001_add_mov_to_l_participants.rb
@@ -1,6 +1,6 @@
 class AddMovToLParticipants < ActiveRecord::Migration[5.2]
   def change
     add_column :league_participants, :mov, :integer
-    add_column :league_participants, :wins, :integer
+    add_column :league_participants, :score, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_125129) do
+ActiveRecord::Schema.define(version: 2019_03_07_214001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,8 @@ ActiveRecord::Schema.define(version: 2019_02_27_125129) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "promotion"
+    t.integer "mov"
+    t.integer "score"
   end
 
   create_table "matches", force: :cascade do |t|
