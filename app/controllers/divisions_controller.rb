13745af145407f020d135c1f08b948dadb2333b7
Alex Raubach <Alex@AlexRaubach.com>
2019-03-20 16:15:23 -0400

    Order players by score > MoV

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -2,6 +2,7 @@ class DivisionsController < ApplicationController
 
   def show
     @division = Division.find(params['id'])
+    @players = @division.league_participants.order(score: :desc, mov: :desc, id: :asc)
   end
 
 end
