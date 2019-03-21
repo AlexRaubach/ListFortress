90f57407115bc6539807225cdd6ba4a2603cd200
Alex Raubach <Alex@AlexRaubach.com>
2019-03-21 15:04:39 -0400

    Show Division name in breadcrumb

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,7 +1,7 @@
 class DivisionsController < ApplicationController
 
   def show
-    @division = Division.find(params['id'])
+    @division = Division.includes(:season).find(params['id'])
     @players = @division.league_participants.includes(:user).order(score: :desc, mov: :desc, id: :asc)
   end
 
