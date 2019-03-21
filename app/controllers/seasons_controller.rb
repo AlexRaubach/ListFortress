a11b0163179dceaed75f666aeaf92b69640f4cfc
Alex Raubach <Alex@AlexRaubach.com>
2019-03-21 11:43:17 -0400

    Eager load divisions and 404 on failure

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,9 +1,24 @@
 class SeasonsController < ApplicationController
+  before_action :set_season, only: :show
+
   def index
     @seasons = Season.all
   end
 
   def show
-    @season = Season.find(params['id'])
+  end
+
+  private
+
+  def set_season
+    @season = Season.includes(:divisions).find(params['id'])
+  rescue ActiveRecord::RecordNotFound
+    render(file: File.join(Rails.root, 'public/404.html'), status: 404, layout: false)
+  # handle not found error
+  rescue ActiveRecord::ActiveRecordError
+    render(file: File.join(Rails.root, 'public/404.html'), status: 404, layout: false)
+  # handle other ActiveRecord errors
+  rescue StandardError
+    render(file: File.join(Rails.root, 'public/404.html'), status: 404, layout: false)
   end
 end
