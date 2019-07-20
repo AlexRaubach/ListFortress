de075e4ebd3968e1a345466adf1d71e7c21f9c17
Alex Raubach <alex@alexraubach.com>
2019-07-19 23:03:04 -0400

    API style fixes

--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -1,66 +1,68 @@
 class Api::V1::TournamentsController < Api::V1::BaseController
-    before_action :load_resource
+  before_action :load_resource
 
-    def index
-        tournaments = @users
+  def index
+    tournaments = @users
 
-        render json: tournaments, each_serializer: Api::V1::TournamentSerializer
+    render json: tournaments, each_serializer: Api::V1::TournamentSerializer
+  end
+
+  def show
+    tournament = Tournament.find(params[:id])
+
+    render json: tournament, include: ['participants', 'rounds', 'rounds.matches'], serializer: Api::V1::TournamentDetailsSerializer
+  end
+
+  private
+  def load_resource
+    validate_params
+    case params[:action].to_sym
+    when :index
+      @users = Tournament.all.updated_after(params[:updatedafter]) # paginate(Tournament.all)
+    when :show
+      @user = Tournament.find(params[:id])
     end
-    def show
-        tournament = Tournament.find(params[:id])
+  end
 
-        render json: tournament, include: ['participants', 'rounds', 'rounds.matches'], serializer: Api::V1::TournamentDetailsSerializer
+  def validate_params
+    if params[:updatedafter].present?
+      if !validate_updated_after(params[:updatedafter])
+        params[:updatedafter] = nil
+        self.api_error(status:404, errors:['invalid updatedafter, requires UTC iso8601 timestamp'])
+      end
     end
+  end
 
-    private
-        def load_resource
-            validate_params()
-              case params[:action].to_sym
-            when :index
-                @users = Tournament.all.updated_after(params[:updatedafter]) #paginate(Tournament.all)
-            when :show
-                @user = Tournament.find(params[:id])
-            end
-        end
+  def validate_updated_after (updated_after)
+    # p updated_after
+    # raises ArgumentError if format invalid
+    Time.iso8601(updated_after)
+    true
+  rescue ArgumentError => e
+    # p e
+    false
+  end
 
-    def validate_params
-        if params[:updatedafter].present?
-            if !validate_updated_after(params[:updatedafter])
-              params[:updatedafter] = nil
-              return self.api_error(status:404,errors:['invalid updatedafter, requires UTC iso8601 timestamp'])
-            end
-        end
-    end
+  def create_params
+    normalized_params.permit(
+      :name, tournament:
+      [
+        :id, :name, :participant_number,
+        :type, :format_id, :country,
+        :state, :organizer_id, :location,
+        :patch_id, :tournament_type_id, :date,
+        :tabletop_url, :cryodex_json, :round_number
+      ]
+    )
+  end
 
-    def validate_updated_after (updated_after)
-        p updated_after
-        Time.iso8601(updated_after)      # raises ArgumentError if format invalid
-        return true
-        rescue ArgumentError => e
-            p e
-            return false
-    end
+  def update_params
+    create_params
+  end
 
-    def create_params
-      normalized_params.permit(
-        :name, tournament:
-        [
-          :id, :name, :participant_number,
-          :type, :format_id, :country,
-          :state, :organizer_id, :location,
-          :patch_id, :tournament_type_id, :date,
-          :tabletop_url, :cryodex_json, :round_number
-        ]
-      )
-    end
-
-    def update_params
-      create_params
-    end
-
-    def normalized_params
-      ActionController::Parameters.new(
-         ActiveModelSerializers::Deserialization.jsonapi_parse(params)
-      )
-    end
+  def normalized_params
+    ActionController::Parameters.new(
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+    )
+  end
 end
