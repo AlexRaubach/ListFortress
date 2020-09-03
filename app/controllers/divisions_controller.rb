fd4bbdade351d695468ed193bfcbc3b713fba9d5
Alex Raubach <alex@alexraubach.com>
2020-09-02 23:45:28 -0400

    Tweak layout and ordering of league participants

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,8 +1,6 @@
 class DivisionsController < ApplicationController
-
   def show
     @division = Division.includes(:season).find(params['id'])
     @players = @division.league_participants.includes(:user).order(score: :desc, mov: :desc, id: :asc)
   end
-
 end
--- app/controllers/league_participant_controller.rb
+++ app/controllers/league_participant_controller.rb
@@ -1,8 +1,12 @@
 class LeagueParticipantController < ApplicationController
   def show
     @league_participant = LeagueParticipant.find(params[:id])
-    @matches = @league_participant.matches.with_attached_log_file
-    @authorized = @league_participant.division.season.active || current_user&.admin
+    @matches = @league_participant.matches
+                                  .includes({ player1: :user }, { player2: :user })
+                                  .with_attached_log_file
+                                  .order(winner_id: :asc, id: :asc)
+
+    @authorized = @league_participant&.division&.season&.active || current_user&.admin
   end
 
   private
