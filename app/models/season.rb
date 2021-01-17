46eea493d4b14f3dbd673b78c3ea0c766437bd64
Alex Raubach <alex@alexraubach.com>
2021-01-16 22:08:06 -0500

    close signups

--- app/models/season.rb
+++ app/models/season.rb
@@ -5,7 +5,7 @@ class Season < ApplicationRecord
 
   CURRENT_SEASON = 11 # used for user.current_league_participant
   CURRENT_SIGNUP_NUMBER = 11 # used in league signup logic i.e. league_signups_controller and league home page
-  SIGNUPS_OPEN = true
+  SIGNUPS_OPEN = false
   INTERDIVISIONAL_ALLOWED = false
 
   def self.add_participant(season_number, user_id, div_tier, div_letter)
