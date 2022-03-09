28234713cd041c23ad7dd7c42e36666e209e305c
Alex Raubach <alex@alexraubach.com>
2022-03-08 21:26:36 -0500

    Depricate formats by setting sort to null

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,3 +1,7 @@
 class Format < ApplicationRecord
   has_many :tournaments
+# This class holds tournament formats
+# The sort field both controls the order they're sorted in but also allows them to be depricated.
+# if sort = null, you can't select them in the tournament create form but can still filter by the format
+# Note that the nulls last sort in app/views/tournaments/_filter.html.erb is postgres only
 end
